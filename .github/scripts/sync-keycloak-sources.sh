c1fdd93a2df5a0a86ad3ec0c4e3e0910bf7e92e5
mposolda <mposolda@gmail.com>
2024-07-24 19:29:09 +0200

    Adding profile 'sync'. Adding script sync-keycloak-sources.sh to trigger syncing Keycloak sources
    Signed-off-by: mposolda <mposolda@gmail.com>

---
+++ .github/scripts/sync-keycloak-sources.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+function echo_header() {
+  echo ""
+  echo "======================================================================="
+  echo "$1"
+  echo "-----------------------------------------------------------------------"
+  echo ""
+}
+
+function error() {
+  echo "======================================================================="
+  echo "Error"
+  echo "-----------------------------------------------------------------------"
+  echo "$1"
+  echo ""
+  exit 1
+}
+
+function syncFiles() {
+  MODULE=$1;
+  echo_header "Syncing files in the module $MODULE";
+  cd $MODULE
+  mvn clean install -Psync
+  mv target/unpacked/* src/main/java/
+
+  if [ -d target/unpacked-resources ]; then
+    mv target/unpacked-resources/* src/main/resources/
+  fi
+  cd ..
+}
+
+# Check if inside keycloak-client directory
+if [[ ! $PWD == *keycloak-client ]]; then
+  error "The script is supposed to be executed in the root of 'keycloak-client' repository";
+fi;
+
+syncFiles admin-client
+syncFiles admin-client-jee
+syncFiles authz-client
+syncFiles policy-enforcer

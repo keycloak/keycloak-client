b6a894d99ca4c3a69caad12d8f914269ab6d612d
mposolda <mposolda@gmail.com>
2024-08-05 15:50:41 +0200

    Testing of Keycloak admin client with Keycloak 24, 25 and nightly. GH actions setup updates to allow caching closes keycloak/keycloak#31868
    Signed-off-by: mposolda <mposolda@gmail.com>

--- testsuite/framework/src/main/java/org/keycloak/client/testsuite/TestConstants.java
+++ testsuite/framework/src/main/java/org/keycloak/client/testsuite/TestConstants.java
@@ -5,6 +5,10 @@ package org.keycloak.client.testsuite;
  */
 public class TestConstants {
 
+    public static final String PROPERTY_KEYCLOAK_LIFECYCLE = "keycloak.lifecycle";
+    public static final String PROPERTY_KEYCLOAK_VERSION = "keycloak.version.docker.image";
+    public static final String KEYCLOAK_VERSION_DEFAULT = "nightly";
+
     public static final String MASTER_REALM = "master";
     public static final String ADMIN_CLI_CLIENT = "admin-cli";
 }

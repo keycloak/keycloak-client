c07994f791ccf2e7cbedc7eac6ac481f7b625176
mposolda <mposolda@gmail.com>
2024-09-09 10:22:46 +0200

    Update snapshot version to 999.0.0-SNAPSHOT similarly like Keycloak has closes #15
    Signed-off-by: mposolda <mposolda@gmail.com>

--- set-version.sh
+++ set-version.sh
@@ -3,6 +3,6 @@
 NEW_VERSION=$1
 
 # Maven
-mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
+mvn versions:set -Pdocs -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DgroupId=org.keycloak* -DartifactId=*
 
 echo "New Mvn Version: $NEW_VERSION" >&2

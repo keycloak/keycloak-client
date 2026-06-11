7cf942d60209b0b503ca4520625fdf7e5f91f717
mposolda <mposolda@gmail.com>
2026-06-11 15:53:30 +0200

    CI not working when repository on other place than in directory keycloak-client closes #219
    Signed-off-by: mposolda <mposolda@gmail.com>

--- testsuite/framework/src/main/java/org/keycloak/client/testsuite/common/TestEnvironment.java
+++ testsuite/framework/src/main/java/org/keycloak/client/testsuite/common/TestEnvironment.java
@@ -9,7 +9,15 @@ public class TestEnvironment {
 
     public static String getProjectRootDir() {
         String userDir = System.getProperty("user.dir");
-        return userDir.substring(0, userDir.lastIndexOf("keycloak-client") + 15);
+        int kcClientIndex = userDir.lastIndexOf("keycloak-client");
+        if (kcClientIndex == -1) {
+            throw new IllegalArgumentException("Not able to find root directory. User dir was: " +userDir);
+        }
+        int projectRootDirIndex = userDir.indexOf(File.separator, kcClientIndex);
+        if (projectRootDirIndex == -1) {
+            throw new IllegalArgumentException("Not able to find file delimited within directory. User dir was: " +userDir);
+        }
+        return userDir.substring(0, projectRootDirIndex);
     }
 
     public static String getTestProvidersFile() {
